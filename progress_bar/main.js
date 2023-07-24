ea3acdb2d6f6962c2ccfe07d10316132ff3afd50
Abhishek Tiwari <103898973+Ajoyabhi@users.noreply.github.com>
2023-07-25 00:51:49 +0530

    first commit

---
+++ progress_bar/main.js
@@ -0,0 +1,44 @@
+const next = document.getElementById('next')
+const prev = document.getElementById('prev')
+const circles = document.querySelectorAll('.circle')
+
+var count = 1
+next.addEventListener('click', () => {
+  count++
+  if (count >= circles.length) {
+    count = circles.length
+  }
+  update()
+})
+
+prev.addEventListener('click', () => {
+  count--
+  if (count < 1) {
+    count = 1
+  }
+  update()
+})
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < count) {
+      circle.classList.add('active')
+    } else {
+      circle.classList.remove('active')
+    }
+  })
+
+  const actives = document.querySelectorAll('.active')
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%'
+
+  if (count === 1) {
+    prev.disabled = true
+  } else if (count === circles.length) {
+    next.disabled = true
+  } else {
+    prev.disabled = false
+    next.disabled = false
+  }
+}
